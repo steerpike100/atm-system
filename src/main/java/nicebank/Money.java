421456711c0e49182d6493d9e227ff799febe847
stephenb <stephen.bartholomew@paxport.net>
2018-05-28 18:02:42 +0100

    third commit

--- src/main/java/nicebank/Money.java
+++ src/main/java/nicebank/Money.java
@@ -1,4 +1,79 @@
 package nicebank;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Money {
-}
+        private final int dollars;
+        private final int cents;
+
+        public Money() {
+            this.dollars = 0;
+            this.cents = 0;
+        }
+
+        public Money(int dollars, int cents) {
+            this.dollars = dollars;
+            this.cents = cents;
+        }
+
+        public Money(String amount) {
+            Pattern pattern = Pattern.compile("^[^\\d]*([\\d]+)\\.([\\d][\\d])$");
+            Matcher matcher = pattern.matcher(amount);
+
+            matcher.find();
+            this.dollars = Integer.parseInt(matcher.group(1));
+            this.cents = Integer.parseInt(matcher.group(2));
+        }
+
+        public int dollars() {
+            return dollars;
+        }
+
+        public int cents() {
+            return cents;
+        }
+
+        public nicebank.Money add(nicebank.Money amount){
+            int newCents = cents + amount.cents();
+            int newDollars = dollars + amount.dollars();
+
+            if (newCents >= 100){
+                newCents -= 100;
+                newDollars++;
+            }
+
+            return new nicebank.Money(newDollars, newCents);
+        }
+
+        public nicebank.Money minus(nicebank.Money amount){
+            int newCents = cents - amount.cents();
+            int newDollars = dollars - amount.dollars();
+
+            if (newCents < 0){
+                newCents += 100;
+                newDollars--;
+            }
+
+            return new nicebank.Money(newDollars, newCents);
+        }
+
+        @Override
+        public boolean equals(Object other){
+            boolean equal = false;
+
+            if (other instanceof nicebank.Money){
+                nicebank.Money otherMoney = (nicebank.Money)other;
+                equal = (this.dollars() == otherMoney.dollars()
+                        && this.cents() == otherMoney.cents());
+            }
+
+            return equal;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("$%01d.%02d", this.dollars(), this.cents());
+        }
+    }
+
--- src/main/java/nicebank/Steps.java
+++ src/main/java/nicebank/Steps.java
@@ -4,17 +4,30 @@ import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.junit.Assert;
 
 public class Steps {
 
     class Account {
-        public Account(int openingBalance) {
+        private Money balance = new Money();
+
+        public void deposit(Money amount) {
+            balance = balance.add(amount);
+        }
+
+        public Money getBalance() {
+            return balance;
         }
     }
 
-    @Given("^I have deposited \\$(\\d+) in my account$")
-    public void iHaveDeposited$InMyAccount(int amount) throws Throwable {
-        new Account(amount);
+    @Given("^I have deposited \\$(\\d+)\\.(\\d+) in my account$")
+    public void iHaveDeposited$InMyAccount(int dollars, int cents) throws Throwable {
+        Account myAccount = new Account();
+        Money amount = new Money(dollars, cents);
+        myAccount.deposit(amount);
+
+        Assert.assertEquals("Incorrect account balance -",
+                amount, myAccount.getBalance());
     }
 
     @When("^I request \\$(\\d+)$")
