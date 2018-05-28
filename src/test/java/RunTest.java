8bb3b92647d95d68e8e167e7245d4954575ef992
stephenb <stephen.bartholomew@paxport.net>
2018-05-28 17:45:18 +0100

    second commit

---
+++ src/test/java/RunTest.java
@@ -0,0 +1,13 @@
+import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        snippets = SnippetType.CAMELCASE,
+        features = "src/test/resources/features",
+        glue = {"nicebank"})
+
+public class RunTest {
+}
